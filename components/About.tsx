05bd3d3c5b4ebb66f85e604ceb78f52d55df6bd3
Anderson Chan <andersonchan4574@gmail.com>
2021-04-08 12:38:59 -0400

    added header to each section

--- components/About.tsx
+++ components/About.tsx
@@ -1,45 +1,81 @@
-import { rootCertificates } from "tls";
+import { rootCertificates } from 'tls'
 
 import React from 'react'
 
 export default function aboutPage() {
   return (
     <div className="about">
-      <div className="about-text">
-        <h2>ABOUT</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <div id="header-about">
+        About <img src="laptop.png" alt="laptop" />
       </div>
-      <div className="about-pictures">
-        <img src="prof.jpeg" alt=""/>
-        {/* <img src="prof2.jpg" alt=""/> */}
-        {/* <img src="dumpling.jpg" alt=""/> */}
-        {/* <img src="prof3.jpeg" alt=""/> */}
+      <div className="about-container">
+        <div className="about-text">
+          <h2>ABOUT</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div className="about-pictures">
+          <img src="prof.jpeg" alt="" />
+          {/* <img src="prof2.jpg" alt=""/> */}
+          {/* <img src="dumpling.jpg" alt=""/> */}
+          {/* <img src="prof3.jpeg" alt=""/> */}
+        </div>
+      </div>
 
-      </div>
       <style jsx>
-      {`
-        .about {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background-color: #D3EFBD;
-          height: 75vh;
-        }
-        .about-text {
-          display: flex;
-          flex-direction: column;
-          color: black;
-          width: 584px;
-        }
-        .about-pictures {
-          display: flex;
-          flex-direction: column;
-          margin-left: 32px;
-          width: 544px;
-
-        }
-      `}
+        {`
+          .about-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #d3efbd;
+            height: 75vh;
+          }
+          .about-text {
+            display: flex;
+            flex-direction: column;
+            color: black;
+            width: 584px;
+          }
+          .about-pictures {
+            display: flex;
+            flex-direction: column;
+            margin-left: 32px;
+            width: 544px;
+          }
+          #header-about {
+            text-align: center;
+            font-size: 50px;
+            font-weight: bold;
+            background-color: #1c2321;
+            width: 100%;
+            padding-bottom: 0.5em;
+            padding-top: 0.2em;
+          }
+          #header-about img {
+            height: 1.5em;
+          }
+        `}
       </style>
     </div>
   )
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
+import ProjectCard from './ProjectCard'
 
-export default class projectsPage extends Component {
+export default class ProjectsPage extends Component {
   render() {
     return (
       <div className="projects">
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div id="header-projects">
+          Projects <img src="laptop.png" alt="laptop" />
+        </div>
+        <div className="projects-container">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
         </div>
 
-
-      <style jsx>
-      {`
-        .projects {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background-color: #D3EFBD;
-          height: 75vh;
-        }
-
-      `}
-      </style>
-    </div>
+        <style jsx>
+          {`
+            .projects-container {
+              display: flex;
+              flex-direction: row;
+            }
+            .projects {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+              background-color: #7d98a1;
+              height: auto;
+            }
+            #header-projects {
+              text-align: center;
+              font-size: 50px;
+              font-weight: bold;
+              background-color: #1c2321;
+              width: 100%;
+              padding-bottom: 0.5em;
+              padding-top: 0.2em;
+            }
+            #header-projects img {
+              height: 1.5em;
+            }
+          `}
+        </style>
+      </div>
     )
   }
 }
